d8a1b526178804ee545775884d9f2ae6a5892234
davidfer1112 <davidfernando1112@gmail.com>
2024-09-21 00:45:16 -0500

    Feature: opcion main

--- src/main/java/org/example/patronadapter/PatronAdapterMain.java
+++ src/main/java/org/example/patronadapter/PatronAdapterMain.java
@@ -51,3 +51,45 @@ public class PatronAdapterMain {
         }
     }
 }
+
+/*
+public class PatronAdapterMain {
+
+
+    public static void main(String[] args) {
+
+        FintechSolicitudCredito request = new FintechSolicitudCredito();
+        request.setCliente("Cristiano Ronaldo");
+        request.setValor(1000);
+
+        InterfaceFintechAdaptador FinancieraB = new AdaptadorFitechB();
+        FintechCreditoRespuesta FBresponse = FinancieraB.EnvioSolicitudCredito(request);
+        System.out.println("Resultado aprobación Financiera <<B>>: > " + FBresponse.isAprobacion() + "\n");
+
+        if (FBresponse.isAprobacion()) {
+            System.out.println("Felicitaciones!!!!!, la Financiera <<B>> aprobó su solicitud de crédito." + "\n");
+        }
+        else
+        {
+            System.out.println("Lo sentimos, la Financiera <<B>> no aprobó su crédito");
+        }
+
+
+
+        InterfaceFintechAdaptador FinancieraA = new AdaptadorFintechA();
+        FintechCreditoRespuesta FAresponse = FinancieraA.EnvioSolicitudCredito(request);
+        System.out.println("Resultado aprobación Financiera <<A>>: > " + FAresponse.isAprobacion() + "\n");
+
+        if (FAresponse.isAprobacion()) {
+            System.out.println("Felicitaciones!!!!!, la Financiera <<A>> aprobó su solicitud de crédito.");
+        }
+        else
+        {
+            System.out.println("Lo sentimos, la Financiera <<A>> no aprobó su crédito");
+        }
+
+    }
+
+}
+
+ */
